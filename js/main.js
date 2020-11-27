217f8e78bb949584a39300b3980fc3007a29616a
Riccardo Ferrari <richi.f93@gmail.com>
2020-11-27 17:26:37 +0100

    final commit

--- js/main.js
+++ js/main.js
@@ -5,33 +5,54 @@ $(document).ready(
 // Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
 // Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
 
-function generateNumber(min, max) {
-  return Math.floor(Math.random()* (max - min) + min);
-};
+    var arrayNumeri = [];
 
-function userPrompt(){
-  var arrayNumeriUtente = [];
-
-  while (arrayNumeri.length < 5) {
-    var numeroCaso = generateNumber(1, 100);
-    if (arrayNumeri.includes(numeroCaso) == false) {
-      arrayNumeri.push(numeroCaso);
+    while (arrayNumeri.length < 5) {
+      var numeroCaso = generateNumber(1, 100);
+      if (arrayNumeri.includes(numeroCaso) == false) {
+        arrayNumeri.push(numeroCaso);
+      }
     }
-  }
-  }
- }
-var arrayNumeri = [];
+    alert(arrayNumeri);
 
-while (arrayNumeri.length < 5) {
-  var numeroCaso = generateNumber(1, 100);
-  if (arrayNumeri.includes(numeroCaso) == false) {
-    arrayNumeri.push(numeroCaso);
-  }
-}
-alert(arrayNumeri);
+    // for (var i = 0; i < array.length; i++) {
+    //   array[i]
+    // }
 
 
-setTimeout(userPrompt, 4000);
+    setTimeout(
+      function () {
+
+        var arrayNumeriUtente = [];
+
+        while (arrayNumeriUtente.length < 5) {
+          var numeroUtente = parseInt(prompt("inserisci un numero da 1 a 100"))
+          if (arrayNumeriUtente.includes(numeroUtente) == false) {
+            arrayNumeriUtente.push(numeroUtente);
+            console.log(arrayNumeriUtente);
+          }
+        };
+
+        var numeriGiusti = [];
+
+        for (var i = 0; i < arrayNumeriUtente.length; i++) {
+          var numero = arrayNumeriUtente[i];
+          if (arrayNumeri.includes(numero) == true) {
+            numeriGiusti.push(numero);
+            console.log(numeriGiusti);
+          }
+        };
+
+        alert(" hai indovinato " + numeriGiusti.length + " e sono " + numeriGiusti )
+
+      }, 4000);
+
+
+    function generateNumber(min, max) {
+      return Math.floor(Math.random()* (max - min) + min);
+    };
+
+
 
 
 
